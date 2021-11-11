8ee08107b698860d59d8968191f31b4c44460ef6
Memotech Bill <64212746+Memotech-Bill@users.noreply.github.com>
2021-11-11 20:36:36 +0000

    Removed BBCSDL and littlefs folders prior to making then sub-modules

--- BBCSDL/include/version.h
+++
@@ -1,9 +0,0 @@
-#define YEAR    "2021"          // Copyright year
-#define VERSION "1.25a"         // Version string
-#define VER_RES 1,25,1,00       // For VERSIONINFO resource
-#define DEFAULT_RAM PAGE_OFFSET+0x200000 // Initial amount of RAM to allocate
-#ifdef __EMSCRIPTEN__
-#define MAXIMUM_RAM 0x1000000   // Maximum amount of RAM to allocate
-#else
-#define MAXIMUM_RAM 0x10000000  // Maximum amount of RAM to allocate
-#endif
