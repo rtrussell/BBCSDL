3602d124be27164ed897555086a3f22ad03c38ab
Richard Russell <richard@rtrussell.co.uk>
2025-04-13 14:04:58 +0100

    v1.41b 2025/04/13: New release

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #define YEAR    "2025"          // Copyright year
-#define VERSION "1.41a"         // Version string
-#define VER_RES 1,41,1,00       // For VERSIONINFO resource
+#define VERSION "1.41b"         // Version string
+#define VER_RES 1,41,2,00       // For VERSIONINFO resource
 #define DEFAULT_RAM PAGE_OFFSET+0x2000000 // Initial amount of RAM to allocate
 #if defined __EMSCRIPTEN__
 #define MAXIMUM_RAM 0x03000000  // Maximum amount of RAM to allocate
